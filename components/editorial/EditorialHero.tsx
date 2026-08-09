3494f6345716db123920ea1cabefd1d23f678944
Otibaco <orjifranklin42@gmail.com>
2026-08-09 15:27:46 +0100

    latest update

--- components/editorial/EditorialHero.tsx
+++ components/editorial/EditorialHero.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-import { SectionLabel } from '@/app/components/editorial/SectionLabel';
+import { SectionLabel } from './SectionLabel';
 
 interface EditorialHeroProps {
   number?: string;
