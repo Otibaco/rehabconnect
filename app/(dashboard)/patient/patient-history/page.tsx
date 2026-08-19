99a0a05ac8fbb1b3896b390e35c22cdc12aa85b3
Otibaco <orjifranklin42@gmail.com>
2026-08-19 14:09:41 +0100

    updated patient route

---
+++ app/(dashboard)/patient/patient-history/page.tsx
@@ -0,0 +1,5 @@
+import { PatientHistoryPage } from "./PatientHistoryPage";
+
+export default function Page() {
+  return <PatientHistoryPage />;
+}
---
+++ app/(dashboard)/patient/patient-resources/page.tsx
@@ -0,0 +1,5 @@
+import { PatientResourcesPage } from "./PatientResourcesPage";
+
+export default function Page() {
+  return <PatientResourcesPage />;
+}
